3ff3a8c3b06010f79e468b4a60d7f60d65c0bdfc
mahruss203 <149577247+mahruss203@users.noreply.github.com>
2024-06-27 16:24:11 -0700

    app.py

--- app.py
+++ app.py
@@ -63,6 +63,7 @@ def predict():
         test_prediction = classifier.predict(X_test)
         test_accuracy = accuracy_score(y_test, test_prediction)
 
-    return render_template('index.html', prediction=predicted_class, train_accuracy=train_accuracy, test_accuracy=test_accuracy)
+    return render_template('predict.html', prediction=predicted_class, train_accuracy=train_accuracy, test_accuracy=test_accuracy)
+
 if __name__ == '__main__':
-    app.run(debug=True,use_reloader=False)
+    app.run(debug=True, use_reloader=False)
